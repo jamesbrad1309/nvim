369d160e744e7803aa5d1279e994c476636714fa
James <james@Jamess-Mac-mini.local>
2025-03-05 23:03:33 +0700

    feat(develop): udpate

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -22,18 +22,46 @@ local on_attach = function(_, bufnr)
 	vim.keymap.set("n", "<leader>rn", '<cmd>Lspsaga rename<CR>', opts)
 end
 
-require('go').setup{
-  lsp_cfg = false
+require('go').setup {
+	lsp_cfg = false
 }
-local cfg = require'go.lsp'.config() -- config() return the go.nvim gopls setup
-
-require('lspconfig').gopls.setup(cfg)
+local cfg = require 'go.lsp'.config() -- config() return the go.nvim gopls setup
+local lspConfig = require("lspconfig")
+lspConfig.gopls.setup(cfg)
 
 require("mason-lspconfig").setup_handlers({
 	function(server_name)
-		lspconfig[server_name].setup({
-			on_attach = on_attach
-		})
+		local genericSettings = {
+			on_attach = on_attach,
+		}
+
+		if server_name == "ts_ls" then
+			print(server_name)
+			local setting = genericSettings
+			setting.settings = {}
+			setting.settings.typescript = {
+				inlayHints = {
+					includeInlayParameterNameHints = "literal",
+					includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+					includeInlayFunctionParameterTypeHints = true,
+					includeInlayVariableTypeHints = false,
+					includeInlayPropertyDeclarationTypeHints = true,
+					includeInlayFunctionLikeReturnTypeHints = true,
+					includeInlayEnumMemberValueHints = true,
+				},
+			}
+			setting.settings.javascript = {
+				inlayHints = {
+					includeInlayParameterNameHints = "all",
+					includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+					includeInlayFunctionParameterTypeHints = true,
+					includeInlayVariableTypeHints = true,
+					includeInlayPropertyDeclarationTypeHints = true,
+					includeInlayFunctionLikeReturnTypeHints = true,
+					includeInlayEnumMemberValueHints = true,
+				}
+			}
+		end
+		lspconfig[server_name].setup(genericSettings)
 	end,
 })
-
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,7 +16,10 @@ return {
 			vim.g.coq_settings = {
 				auto_start = true,
 			}
-		end
+		end,
+		opt = {
+			inlay_hints = { enabled = true }
+		}
 	},
 	"nvim-treesitter/nvim-treesitter",
 	{
@@ -112,5 +115,114 @@ return {
 		event = { "CmdlineEnter" },
 		ft = { "go", 'gomod' },
 		build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-	}
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		keys = {
+			{
+				";c",
+				":LazyGit<Return>",
+				silent = true,
+				noremap = true,
+			},
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod",                     lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+		keys = {
+			{
+
+				"<leader>d",
+				"<cmd>NvimTreeClose<cr><cmd>tabnew<cr><bar><bar><cmd>DBUI<cr>",
+			},
+		},
+	},
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 5000,
+			background_colour = "#000000",
+			render = "wrapped-compact",
+		},
+	},
+	{
+		"folke/noice.nvim",
+		opts = function(_, opts)
+			local focused = true
+			vim.api.nvim_create_autocmd("FocusGained", {
+				callback = function()
+					focused = true
+				end,
+			})
+			vim.api.nvim_create_autocmd("FocusLost", {
+				callback = function()
+					focused = false
+				end,
+			})
+
+			opts.commands = {
+				all = {
+					-- options for the message history that you get with `:Noice`
+					view = "split",
+					opts = { enter = true, format = "details" },
+					filter = {},
+				},
+			}
+		end,
+	},
+	{
+		"b0o/incline.nvim",
+		dependencies = {},
+		event = "BufReadPre",
+		priority = 1200,
+		config = function()
+			local helpers = require("incline.helpers")
+			require("incline").setup({
+				window = {
+					padding = 0,
+					margin = { horizontal = 0 },
+				},
+				render = function(props)
+					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+					local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
+					local modified = vim.bo[props.buf].modified
+					local buffer = {
+						ft_icon and
+						{
+							" ",
+							ft_icon,
+							" ",
+							guibg = ft_color,
+							guifg = helpers.contrast_color(
+								ft_color)
+						}
+						or "",
+						" ",
+						{ filename, gui = modified and "bold,italic" or "bold" },
+						" ",
+						guibg = "#363944",
+					}
+					return buffer
+				end,
+			})
+		end,
+	},
 }
